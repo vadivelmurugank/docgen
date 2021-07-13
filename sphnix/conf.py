c738e466ef7712f204355f59451b93de419f05e0
Vadivel Kannappa <vadivel.kannappa@intel.com>
2021-07-12 17:40:12 -0700

    updates templates

--- sphnix/conf.py
+++ sphnix/conf.py
@@ -129,9 +129,19 @@ import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme_options = {
-    'collapse_navigation': False,
+	'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
     'navigation_depth': 4,
-    'sticky_navigation': False
+    'includehidden': True,
+    'titles_only': False
 }
 
 #html_theme_options = {
